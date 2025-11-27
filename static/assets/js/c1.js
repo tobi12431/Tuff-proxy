ad4e5ad0575b5086fb9ae745b9a5d396ecefe99a
tobi12431 <tobiboi114@gmail.com>
2025-11-26 22:37:28 -0500

    Remove category filter and search functionality
    Removed the filtering logic for categories and the search functionality.

--- static/assets/js/c1.js
+++ static/assets/js/c1.js
@@ -383,29 +383,6 @@ function category() {
   const selectedCategories = Array.from(document.querySelectorAll("#category option:checked")).map(option => option.value);
   const g = document.getElementsByClassName("column");
 
-  for (const game of g) {
-    const categories = game.getAttribute("data-category").split(" ");
-
-    if (selectedCategories.length === 0 || selectedCategories.some(category => categories.includes(category))) {
-      game.style.display = "block";
-    } else {
-      game.style.display = "none";
-    }
-  }
-}
-
-function bar() {
-  const input = document.getElementById("search");
-  const filter = input.value.toLowerCase();
-  const g = document.getElementsByClassName("column");
-
-  for (const game of g) {
-    const name = game.getElementsByTagName("p")[0].textContent.toLowerCase();
-
-    if (name.includes(filter)) {
-      game.style.display = "block";
-    } else {
-      game.style.display = "none";
     }
   }
 }
